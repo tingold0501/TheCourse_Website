7f838936bbcef01f647b24c2cf35b07e79065913
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2023-11-17 10:12:27 +0700

    ....

---
+++ thecourse/app/Http/Controllers/USerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserM;
+use Illuminate\Http\Request;
+
+class USerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\UserM  $userM
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UserM $userM)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\UserM  $userM
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UserM $userM)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\UserM  $userM
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UserM $userM)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\UserM  $userM
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UserM $userM)
+    {
+        //
+    }
+}
